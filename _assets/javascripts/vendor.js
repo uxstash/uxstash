068b8fb9a0c07f85f6bbefb17c1b824d8dd2c9a8
Alex Kale <hi@awkale.me>
2016-02-14 12:33:06 -0500

    complete animated cards

--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,4 +1,5 @@
 //= require jquery/dist/jquery
 //= require Materialize/dist/js/materialize.js
+//= require animated_grid/modernizr.custom.js
 //= require animated_grid/classie.js
 //= require animated_grid/animated.main.js
