241b20a28cc87c96032bdc1ef11e58effb43928d
Dan Calise <calise@gmail.com>
2018-10-08 16:05:28 -0400

    adding router and basic components

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="container">
+          ux stash
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Home;
