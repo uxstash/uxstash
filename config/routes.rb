6de45c2788c8c7549d1cbadb68a5f6fabd05872e
Alex Kale <hi@awkale.me>
2018-10-21 12:15:23 -0400

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/new'
+  get 'articles/show'
+  get 'articles/create'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :articles
+    resources :categories
+    resources :stashes
+  end
 end
