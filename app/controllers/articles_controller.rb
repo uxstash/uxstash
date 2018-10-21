d34be5c84cf0ba90c47321c3a3d9dd6d7ff2ef8d
Alex Kale <hi@awkale.me>
2018-10-21 12:15:07 -0400

    add controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, :only => [:show, :update, :destroy]
+
+  def index
+    @articles = Article.all
+    json_response(@articles)
+  end
+
+  def show
+    json_response(@article)
+  end
+
+  def create
+    @article = Article.create!(article_params)
+    json_response(@article, :created)
+  end
+
+  def update
+    @article.update(article_params)
+    head :no_content
+  end
+
+  def destroy
+    @article.destroy
+    head :no_content
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :author, :url, :date)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+end
