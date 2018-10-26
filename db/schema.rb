e2c158254d2f0aa4ca64dc63f11957b828de98d9
Alex Kale <hi@awkale.me>
2018-10-26 16:05:29 -0400

    adding seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_161426) do
+ActiveRecord::Schema.define(version: 2018_10_26_194750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2018_10_16_161426) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.integer "stash_id"
   end
 
   create_table "categories", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,275 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Article.create([{
-  title: 'I am an article title',
-  author: 'john doe',
-  url: 'www.nytimes.com',
-  date: '2015-12-08'
+# Article.create([{
+#   title: 'I am an article title',
+#   author: 'john doe',
+#   url: 'www.nytimes.com',
+#   date: '2015-12-08'
+# },
+# {
+#   title: 'I am an article title 2',
+#   author: 'jane doe',
+#   url: 'www.cnn.com',
+#   date: '2015-12-08'
+# },
+# {
+#   title: 'I am an article title 3',
+#   author: 'john doe',
+#   url: 'www.nytimes.com',
+#   date: '2015-12-08'
+# }
+# ])
+#
+Category.create([{
+  name: 'Strategy ',
+  description: 'Objectives & Needs'
 },
 {
-  title: 'I am an article title 2',
-  author: 'jane doe',
-  url: 'www.cnn.com',
-  date: '2015-12-08'
+  name: 'Management ',
+  description: 'Specs & Requirements'
 },
 {
-  title: 'I am an article title 3',
-  author: 'john doe',
-  url: 'www.nytimes.com',
-  date: '2015-12-08'
+  name: 'Architecture',
+  description: 'Structure & Interaction'
+},
+{
+  name: 'Development',
+  description: 'Code & Navigation'
+},
+{
+  name: 'Design',
+  description: 'Visual & Interactive'
 }
+])
+
+Stash.create([{
+  name: '80/20 rule ',
+  description: 'The Pareto principle (also known as the 80–20 rule, the law of the vital few, and the principle of factor sparsity) states that, for many events, roughly 80% of the effects come from 20% of the causes.'
+},
+{
+  name: 'AB / Multivariate Testing',
+  description: 'A technique for testing a hypothesis in which multiple variables are modified. The goal of multivariate testing is to determine which combination of variations performs the best out of all of the possible combinations.'
+},
+{
+  name: 'Accessibility Analysis ',
+  description: 'A study to measure how the website is easy for everyone to use, regardless of their special needs. It could be done by specific tools, following the W3C guideline or even by an usability test.'
+},
+{
+  name: 'Adaptive Web Design (AWD)',
+  description: 'Adaptive websites adapt to the width of the browser at a specific points. In other words, the website is only concerned about the browser being a specific width, at which point it adapts the layout.'
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+{
+  name: '',
+  description: ''
+},
+
 ])
