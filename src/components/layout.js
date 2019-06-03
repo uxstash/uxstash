23f591a02536f01f46f4eed7d857cd56c06d292d
Alex Kale <hi@awkale.me>
2019-06-03 18:29:49 -0400

    test call to contentful with graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import './layout.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -21,33 +21,51 @@ const Layout = ({ children }) => (
             title
           }
         }
+        allContentfulCategory {
+          edges {
+            node {
+              name
+              shortDescription {
+                shortDescription
+              }
+            }
+          }
+        }
       }
     `}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
+        <ul>
+          {data.allContentfulCategory.edges.map(category => (
+            <li>{category.node.name}</li>
+          ))}
+        </ul>
         <div
           style={{
-            margin: `0 auto`,
+            margin: '0 auto',
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
+            © 
+{' '}
+{new Date().getFullYear()}
+, Built with
+{" "}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
         </div>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
