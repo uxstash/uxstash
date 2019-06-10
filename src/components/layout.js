aad2aa9aad895c075ba4fe0ebb186dbfff442863
Alex Kale <hi@awkale.me>
2019-06-10 11:13:03 -0400

    move content to new components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-
-import Header from './header';
-import './layout.css';
+import Wrapper from './Wrapper';
+import Header from './Header';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -21,45 +13,19 @@ const Layout = ({ children }) => (
             title
           }
         }
-        allContentfulCategory {
-          edges {
-            node {
-              name
-              shortDescription {
-                shortDescription
-              }
-            }
-          }
-        }
       }
     `}
     render={data => (
-      <>
+      <Wrapper>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <ul>
-          {data.allContentfulCategory.edges.map(category => (
-            <li>{category.node.name}</li>
-          ))}
-        </ul>
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <main>{children}</main>
           <footer>
-            © 
-{' '}
-{new Date().getFullYear()}
-, Built with
-{" "}
+            {`© ${new Date().getFullYear()}, Built with`}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
         </div>
-      </>
+      </Wrapper>
     )}
   />
 );
