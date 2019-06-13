80fc9d85ebe3bc2aecfa41d833fb5b7007c72806
Alex Kale <hi@awkale.me>
2019-06-12 22:20:08 -0400

    remove old content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,10 +20,7 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div>
           <main>{children}</main>
-          <footer>
-            {`© ${new Date().getFullYear()}, Built with`}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          <footer />
         </div>
       </Wrapper>
     )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,6 @@ function IndexPage({ data }) {
           />
         ))}
       </GridWrapper>
-      <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   );
 }
