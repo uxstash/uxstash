0769aaeb4eb53fb1c8dbb690daf8149a9ae65789
Alex Kale <hi@awkale.me>
2019-06-12 21:50:59 -0400

    fix gutters

--- src/components/styles/GridWrapper.js
+++ src/components/styles/GridWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const GridWrapper = styled.div`
   display: grid;
-  grid-gap: 40px;
+  grid-gap: 20px;
   @media (min-width: ${props => props.theme.md}) {
     grid-template-columns: 1fr 1fr 1fr;
   }
