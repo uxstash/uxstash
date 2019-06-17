f3e500e617ee552dd44ca9c27d3b8a8ab1b3ce75
Alex Kale <hi@awkale.me>
2019-06-17 11:36:36 -0400

    create pages dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,38 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    const template = path.resolve('src/templates/stash.js');
+    resolve(
+      graphql(`
+        {
+          allContentfulStash(filter: { slug: { ne: null } }) {
+            edges {
+              node {
+                slug
+              }
+            }
+          }
+        }
+      `).then((result) => {
+        if (result.errors) {
+          reject(result.erros);
+        }
+        result.data.allContentfulStash.edges.forEach((stash) => {
+          createPage({
+            path: `stash/${stash.node.slug}`,
+            component: template,
+            context: {
+              slug: stash.node.slug,
+            },
+          });
+        });
+        return;
+      }),
+    );
+  });
+};
