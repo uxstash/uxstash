6d5fff571829532a6064f697412e0c7743bc57e5
Alex Kale <hi@awkale.me>
2019-06-20 15:10:13 -0400

    add hotjar

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-hotjar',
+      options: {
+        id: 809689,
+        sv: 6,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
