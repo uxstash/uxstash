47047bd8f010bd99a15451bd219624624aa7d174
Alex Kale <hi@awkale.me>
2019-07-07 13:02:47 -0400

    add redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-plugin-hotjar',
       options: {
